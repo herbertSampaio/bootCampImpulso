13b11ebc3bb32a67ac60861807228a5947d8a3c5
Herbert Sampaio <herbert.sampaio@squadra.com.br>
2019-04-12 18:22:49 -0400

    Posicionamento do header

--- app/src/Components/HeaderWrapper/index.js
+++ app/src/Components/HeaderWrapper/index.js
@@ -4,9 +4,7 @@ const HeaderWrapper = styled.header`
     background-color: #26265e;
     padding:10px 16px;
     display:flex;
-    flex-wrap: wrap;
     justify-content: space-between;
-    flex-direction: row; 
 `;
 
 export default HeaderWrapper;
