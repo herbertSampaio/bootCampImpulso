136457eb0316e907e95d2feaf9d0ca46fa107bfd
Herbert Sampaio <herbert.sampaio@squadra.com.br>
2019-04-15 09:45:54 -0400

    Desafio top top top

---
+++ app/src/Components/BtnMenssage/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const BtnMessage = styled.input.attrs({
+    placeholder:'Diz aí...'
+})`
+    width: 375px;
+    height: 50px;
+    margin-top:50px;
+`;
+
+export default BtnMessage;
--- app/src/Pages/Chat/index.js
+++ app/src/Pages/Chat/index.js
@@ -4,12 +4,16 @@ import ChatWrapper from '../../Components/ChatWrapper';
 import ImgBotCamp from '../../Components/ImgBotCamp';
 import ImgLogout from '../../Components/ImgLogout';
 import HeaderWrapper from '../../Components/HeaderWrapper';
+import BtnMessage from '../../Components/BtnMenssage';
 
 const Chat = () => (
-    <HeaderWrapper>
-        <ImgBotCamp alt="img botcamp" small/>
-        <ImgLogout alt="img logout"/>
-    </HeaderWrapper>
+    <>
+        <HeaderWrapper>
+            <ImgBotCamp alt="img botcamp" small />
+            <ImgLogout alt="img logout" />
+        </HeaderWrapper>
+        <BtnMessage />
+    </>
 );
 
 export default Chat;
