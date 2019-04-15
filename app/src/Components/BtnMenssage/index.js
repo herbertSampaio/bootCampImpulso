bfd3748559e413155d9fc036da30535d2090549f
Herbert Sampaio <herbert.sampaio@squadra.com.br>
2019-04-15 10:03:03 -0400

    Btn input message

--- app/src/Components/BtnMenssage/index.js
+++ app/src/Components/BtnMenssage/index.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
 const BtnMessage = styled.input.attrs({
-    placeholder:'Diz aí...'
+    placeholder:'Diz aí...',
+    type:"Text"
 })`
-    width: 375px;
+    width: 100vw;
     height: 50px;
-    margin-top:50px;
+    box-sizing:border-box;
+    background-color:#fbfbff
+    box-shadow: 0 -2px 8px 0 rgba(0, 0, 0, 0.1);
+    font-size: 20px;
+    color: rgba(0, 0, 0, 0.33);
+    position:absolute;
+    bottom:0;
+    border:none;
+    padding:14px 11px 14px 16px;
 `;
 
 export default BtnMessage;
